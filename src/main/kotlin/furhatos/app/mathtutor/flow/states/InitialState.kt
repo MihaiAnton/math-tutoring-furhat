6702732c3ec892866ea0f27a9631330c48af5fbd
Mihai Anton <antonmihai58@gmail.com>
2020-10-14 19:22:43 +0200

    Adds default onEntry action

--- src/main/kotlin/furhatos/app/mathtutor/flow/states/InitialState.kt
+++ src/main/kotlin/furhatos/app/mathtutor/flow/states/InitialState.kt
@@ -1,6 +1,14 @@
 package furhatos.app.mathtutor.flow.states;
 
+import furhatos.app.mathtutor.flow.CustomGaze
+import furhatos.flow.kotlin.furhat
 import furhatos.flow.kotlin.state
 
 val InitialState = state {
+    onEntry {
+        parallel {
+            goto(CustomGaze)
+        }
+        furhat.say("Initial State")
+    }
 }
--- src/main/kotlin/furhatos/app/mathtutor/flow/states/addition/WrongAddition.kt
+++ src/main/kotlin/furhatos/app/mathtutor/flow/states/addition/WrongAddition.kt
@@ -1,8 +1,15 @@
 package furhatos.app.mathtutor.flow.states.addition
 
+import furhatos.app.mathtutor.flow.CustomGaze
 import furhatos.flow.kotlin.furhat
 import furhatos.flow.kotlin.onResponse
 import furhatos.flow.kotlin.state
 
 val WrongAddition = state {
+    onEntry {
+        parallel {
+            goto(CustomGaze)
+        }
+        furhat.say("Wrong Addition")
+    }
 }
