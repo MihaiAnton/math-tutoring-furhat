63d83d655881f80bf4473d9892725bd5e9440cdd
JaspervanTilburg <jaspert95@gmail.com>
2020-10-19 18:10:14 +0200

    Add api request for emotion recognition

--- src/main/kotlin/furhatos/app/mathtutor/users.kt
+++ src/main/kotlin/furhatos/app/mathtutor/users.kt
@@ -1,3 +1,7 @@
 package furhatos.app.mathtutor
 
+import furhatos.flow.kotlin.NullSafeUserDataDelegate
+import furhatos.records.User
 
+var User.valence : Double? by NullSafeUserDataDelegate { 0.0 }
+var User.arousal : Double? by NullSafeUserDataDelegate { 0.0 }
