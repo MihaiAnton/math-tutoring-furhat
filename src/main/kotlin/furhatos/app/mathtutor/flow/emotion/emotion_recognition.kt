abbd1f14d65bfec87f6c3fc51800d82bb7e00d57
JaspervanTilburg <jaspert95@gmail.com>
2020-10-19 18:10:14 +0200

    Add api request for emotion recognition

---
+++ src/main/kotlin/furhatos/app/mathtutor/flow/emotion/emotion_recognition.kt
@@ -0,0 +1,17 @@
+package furhatos.app.mathtutor.flow.emotion
+
+import furhatos.app.mathtutor.arousal
+import furhatos.app.mathtutor.valence
+import furhatos.flow.kotlin.State
+import furhatos.flow.kotlin.state
+import furhatos.flow.kotlin.users
+
+fun getEmotionFromApi() : State = state {
+    onEntry {
+        val response = khttp.get("http://127.0.0.1:5000/get_emotion").text
+        val values = response.split(' ')
+        users.current.valence = values[0].toDouble()
+        users.current.arousal = values[1].toDouble()
+        println("alence: ${users.current.valence}, arousal: ${users.current.arousal}")
+    }
+}
