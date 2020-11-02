53dac09a169f7ef0c1832c87c95fbecca27906e3
JaspervanTilburg <jaspert95@gmail.com>
2020-11-02 22:11:21 +0100

    Remove old import

--- src/main/kotlin/furhatos/app/mathtutor/flow/mathtutor.kt
+++ src/main/kotlin/furhatos/app/mathtutor/flow/mathtutor.kt
@@ -14,7 +14,6 @@ import furhatos.app.mathtutor.flow.states.excercises.ExercisesWrong2
 import furhatos.app.mathtutor.flow.states.excercises.StartExercises
 import furhatos.app.mathtutor.flow.states.multiplication.*
 import furhatos.app.mathtutor.nlu.*
-import furhatos.app.spaceshipattendant.flow.gaze.RuleBasedGaze
 import furhatos.flow.kotlin.*
 import furhatos.nlu.common.*
 
