d12582befa0880c9f3d0a649c6088a36abbc9792
Mihai Anton <antonmihai58@gmail.com>
2020-11-03 16:24:57 +0100

    Adds missing closing bracket

--- src/main/kotlin/furhatos/app/mathtutor/flow/states/excercises/WrongExplanation2.kt
+++ src/main/kotlin/furhatos/app/mathtutor/flow/states/excercises/WrongExplanation2.kt
@@ -23,13 +23,14 @@ fun WrongExplanation2(subject: String?): State = state(Interaction) {
             furhat.say("Wrong Explanation 2")
         } else {
             random(
-                    {furhat.say("That is not correct. Let's go through the explanation first.")},
-                    {furhat.say("It seems to me that you need to freshen up your $subject skills.")},
-                    {furhat.say("That wasn't the answer I was looking for. Time for some explanation.")},
-                    {furhat.say("Perhaps you'll do better after going through the explanation.")}
+                    { furhat.say("That is not correct. Let's go through the explanation first.") },
+                    { furhat.say("It seems to me that you need to freshen up your $subject skills.") },
+                    { furhat.say("That wasn't the answer I was looking for. Time for some explanation.") },
+                    { furhat.say("Perhaps you'll do better after going through the explanation.") }
             )
 
-        delay(1000)
-        goto(StartExplanation(subject))
+            delay(1000)
+            goto(StartExplanation(subject))
+        }
     }
 }
